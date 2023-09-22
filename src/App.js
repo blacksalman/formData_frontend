79ee6b0c05dbc5ac6e6af76fe17ff1a20b0530af
blacksalman <salman51180189@gmail.com>
2023-09-22 10:55:04 +0530

    formData frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [form, setForm] = useState({});
+  const [users, setUsers] = useState([]);
+
+  const handleForm = (e) =>{
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    const response = await fetch('http://localhost:8000/userForm', {
+      method: 'POST',
+      body: JSON.stringify(form),
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    return await response.json();
+  
+  }
+
+  const getUser = async() =>{
+    const response =  await fetch('http://localhost:8000/userForm',{
+      method: 'GET'
+    })
+    
+    const data = await response.json();
+    setUsers(data);
+  }
+
+  useEffect(()=>{
+    getUser();
+  },[users])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <span>userName</span>
+        <input type='text' name='username' onChange={handleForm}/>
+        <span>password</span>
+        <input type='text' name='password'  onChange={handleForm}/>
+        <input type='submit'/>
+      </form>
+      <div>
+        <ul>
+          {users.map(user=><li key={user._id}>{user.username}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
